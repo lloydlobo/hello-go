96de822a259d6c91c3ce8ce2c67b0b71f33b50db
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-09-09 17:52:07 +0530

    feat: init go mod local dependency

--- hello.go
+++ hello.go
@@ -1,10 +1,20 @@
 package main
 
-import "fmt"
-
-import "rsc.io/quote"
+// import "fmt"
+import (
+    "fmt"
+   "example.com/greetings"
+)
 
 func main() {
+    // Get a greeting message and print it
+    message := greetings.Hello("Gladys")
+
+    fmt.Println(message)
+}
+
+// import "rsc.io/quote"
+/* func main_quote() {
     fmt.Println("Hello, world!")
     fmt.Println(quote.Go())
-}
+} */
